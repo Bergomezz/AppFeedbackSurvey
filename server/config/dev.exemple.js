f09f312c26c766090261f10cbf6f1a6849427428
Bergomezz <bernardogomezz@yahoo.com.br>
2023-05-04 15:10:53 -0300

    finishing the oauth and the setting of railway configuration

---
+++ server/config/dev.exemple.js
@@ -0,0 +1,13 @@
+module.exports = {
+  googleClientID: 'your google cloud client ID',
+  googleClientSecret: 'your google cloud client secret',
+  callbackURL: '/auth/google/callback',
+  mongoURI: 'you mongoDb link',
+  cookieKey: 'something something',
+};
+
+//mongoS: ZRXXQBHvYeabNVMw
+//mongoDb: mongodb+srv://bernardogomezz:<password>@cluster0.acdlf8w.mongodb.net/?retryWrites=true&w=majority
+
+//goodleID:633928383809-vmqmdhel0eoalj3deslgr4o616b370ku.apps.googleusercontent.com
+//googleS: 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
--- server/config/keys.exemple.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: 'your google cloud client ID',
-  googleClientSecret: 'your google cloud client secret',
-  callbackURL: '/auth/google/callback',
-  mongoURI: 'you mongoDb link',
-};
