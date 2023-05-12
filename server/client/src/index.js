d9f31795e5838c86abf28d530ffa84260bad757a
Bergomezz <bernardogomezz@yahoo.com.br>
2023-05-12 11:41:32 -0300

    fixing the problem with the Provider

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App.js';
@@ -9,8 +9,8 @@ import reducers from './reducers';
 const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
-  <App>
-    <Provider store={store}></Provider>
-  </App>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.querySelector('#root')
 );
