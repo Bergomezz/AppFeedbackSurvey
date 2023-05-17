48a2beb8f7ef9fc454b6ed7396db619d87cc7eea
Bergomezz <bernardogomezz@yahoo.com.br>
2023-05-17 15:13:19 -0300

    create action folder inside the src with 2 files, and starting refactoring the class App components

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div>
-        <div>Header</div>
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="left brand-logo">
+            FeedbackAppSurvey
+          </a>
+          <ul className="right">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
