c8f6da7bdf2b365e39e3fb843191a1d116cb4fe3
Bergomezz <bernardogomezz@yahoo.com.br>
2023-06-12 14:47:38 -0300

    starting organize and add some logic on the survey routes

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin.js');
 const requireCredits = require('../middlewares/requireCredits.js');
 
+const Survey = mongoose.model('surveys');
+
 module.export = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {});
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+    });
+  });
 };
