fdcc3a6ddcff7bad61bac6bbee11218388e88c1a
Bergomezz <bernardogomezz@yahoo.com.br>
2023-06-21 15:06:07 -0300

    testing the send grid email

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -8,6 +8,11 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App.js';
 import reducers from './reducers';
 
+// Development only axios helpers
+// TODO delete later!
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
