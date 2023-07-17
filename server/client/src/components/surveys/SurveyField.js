968df6e1729135388745c68ed608ab8ab68d14fa
Bergomezz <bernardogomezz@yahoo.com.br>
2023-07-17 19:16:35 -0300

    adding the survey form and logic

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -1,6 +1,11 @@
 // SurveyField contaisn logic to render a sengle label and text input
 import React from 'react';
 
-export default () => {
-  return <div> SurveyField</div>;
+export default ({ input, label }) => {
+  return (
+    <div>
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
 };
