7bb281efa830efd6e999f3d694a997acade97ee9
Bergomezz <bernardogomezz@yahoo.com.br>
2023-07-19 18:27:05 -0300

    initial configuration of survey form review

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,1 +1,21 @@
 // logic to survey form review
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SurveyReview = ({ formData }) => {
+  return (
+    <div>
+      <h5>Please confirm your entries</h5>
+      <Link to="/surveys/new" className="red btn-flat left white-text">
+        Back
+        <i className="material-icons right">back</i>
+      </Link>
+      <button type="submit" className="teal btn-flat right white-text">
+        Submit
+        <i className="material-icons right">done</i>
+      </button>
+    </div>
+  );
+};
+
+export default SurveyReview;
--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -2,8 +2,11 @@
 
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm.js';
+import SurveyFormReview from './SurveyFormReview.js';
 
 class SurveyNew extends Component {
+  state = { formReview: false };
+
   render() {
     return (
       <div>
