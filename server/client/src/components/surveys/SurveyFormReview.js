975995895d0c6269ae9f26ea3aa0655b921d2990
Bergomezz <bernardogomezz@yahoo.com.br>
2023-07-25 11:45:47 -0300

    refactoring the survey forms and add the review form before the send final task

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,18 +1,33 @@
 // SurveyFromReview shows users their form inputs for review
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields.js';
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your entries</h5>
-      <button className="yellow darken-3 btn-flat left " onClick={onCancel}>
+      {reviewFields}
+      <button
+        className="yellow darken-3 btn-flat left white-text"
+        onClick={onCancel}
+      >
         Back
-        <i className="darken-3 material-icons right ">back</i>
+        <i className="material-icons right ">cancel</i>
       </button>
-      <button type="submit" className="teal btn-flat right white-text">
-        Submit
-        <i className="material-icons right">done</i>
+      <button type="submit" className="green btn-flat right white-text">
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
